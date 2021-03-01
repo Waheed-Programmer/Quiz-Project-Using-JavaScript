f19c2803f3ec553f371104201601dd96b5021d3e
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-03-01 21:48:33 +0500

    Quiz Project Html CSS and JavaScript

---
+++ js/question.js
@@ -0,0 +1,81 @@
+var question = [{
+    "question": "The first mechanical computer designed by charles Babbage was called ?",
+    "option1" : "Abacus",
+    "option2" : "Analytical Engine",
+    "option3" : "Calculator",
+    "option4" : "Processor",
+    "answer" : "2"
+
+},{
+    "question": "Which of the following is the most powerful type of Computer ?",
+    "option1" : "Super-micro",
+    "option2" : "Super conductor",
+    "option3" : "Super Computer",
+    "option4" : "Magaframe",
+    "answer" : "3"
+
+},{
+    "question": "Which is the single integrated circuit?",
+    "option1" : "Gate",
+    "option2" : "Mother Board",
+    "option3" : "Chip",
+    "option4" : "CPU",
+    "answer" : "1"
+
+},{
+    "question": "C is  ?",
+    "option1" : "A third generation high level language",
+    "option2" : "A michine language",
+    "option3" : "An assembly language",
+    "option4" : "all of the above",
+    "answer" : "1"
+
+},{
+    "question": "Web pages are written using ?",
+    "option1" : "FTP",
+    "option2" : "HTTP",
+    "option3" : "HTML",
+    "option4" : "URL",
+    "answer" : "3"
+
+},{
+    "question": "Which of the following is an example of none volatile memory ?",
+    "option1" : "VLSI",
+    "option2" : "LSI",
+    "option3" : "ROM",
+    "option4" : "RAM",
+    "answer" : "3"
+
+},{
+    "question": "UNIVAC is an example of",
+    "option1" : "First generation computer",
+    "option2" : "Second generation computer",
+    "option3" : "Third generation computer",
+    "option4" : "Fourth generation computer",
+    "answer" : "1"
+
+},{
+    "question": "Graphic interface were first use in xerox product is called?",
+    "option1" : "VLSI",
+    "option2" : "LSI",
+    "option3" : "ROM",
+    "option4" : "RAM",
+    "answer" : "1"
+
+},{
+    "question": "Find the odd one out ?",
+    "option1" : "ORACLE",
+    "option2" : "SYBASE",
+    "option3" : "C",
+    "option4" : "INFURRIA",
+    "answer" : "3"
+
+},{
+    "question": "UNIVAC is an example of",
+    "option1" : "First generation computer",
+    "option2" : "Second generation computer",
+    "option3" : "Third generation computer",
+    "option4" : "Fourth generation computer",
+    "answer" : "1"
+
+}]
